2b2cc3bc66e0b39e3801f6dfdd4a79644d76494d
Howard Kwon <howie.ykwon70@gmail.com>
2021-07-19 20:25:40 -0400

    commit tree 146f08ecda3dd6970fea0d9f3d512b2051de8572

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+function saturdayFun(activity='roller-skate') {
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+
+function mondayWork(adulting='go to the office') {
+    return(`This Monday, I will ${adulting}.`)
+}
+
+
+function wrapAdjective(flair='*') {
+    return function(emphatic='a dedicated programmer'){
+    return `You are ${flair}${emphatic}${flair}!`;
+    }
+}
